43616afbcda3d837223364e8284460c4cac47c21
SheihMaks <92758256+SheihMaks@users.noreply.github.com>
2022-07-07 22:37:00 +0300

    +

--- src/components/userFeedback/Feedback.jsx
+++ src/components/userFeedback/Feedback.jsx
@@ -1,21 +1,34 @@
 import React from 'react'
 import PropTypes from 'prop-types';
-import {FeedBackMain,FeedBackContainer,ButtonsDiv,Button} from './Feedback.styled'
+import {FeedBackMain,FeedBackContainer,MainMessage,ButtonsDiv,Button,StatisticsContainer,StatisticsHeader,Statistics,StatisticsItem} from './Feedback.styled'
 export class FeedbackComponent extends React.Component{
+    // static defaultProps = {
+    //     igood: 2,
+    // ineutral: 2,
+    // ibad: 2
+    //   };
     state={
-    ngood: 0,
+    good: 0,
     neutral: 0,
     bad: 0
     }
     render(){
         return(<FeedBackMain><FeedBackContainer>
-            <h1>Please leave feedback</h1>
+            <MainMessage>Please leave feedback</MainMessage>
             <ButtonsDiv>
-                <Button>Good</<Button>
-                <<Button>Neutral</<Button>
-                <<Button>Bad</<Button>
+                <Button>Good</Button>
+                <Button>Neutral</Button>
+                <Button>Bad</Button>
             </ButtonsDiv>
-
-            </FeedBackContainer></FeedBackMain>)
+            <StatisticsContainer>
+                <StatisticsHeader>Statistics</StatisticsHeader>
+                <Statistics>
+                    <StatisticsItem>Good: {this.state.good}</StatisticsItem>
+                    <StatisticsItem>Neutral: {this.state.neutral}</StatisticsItem>
+                    <StatisticsItem>Bad: {this.state.bad}</StatisticsItem>
+                </Statistics>
+            </StatisticsContainer>
+</FeedBackContainer></FeedBackMain>
+            )
     }
 }
--- src/components/userFeedback/Feedback.styled.js
+++ src/components/userFeedback/Feedback.styled.js
@@ -1,14 +1,33 @@
 import styled from "styled-components"
 
 export const FeedBackMain=styled.main`
-width:100%;`
+width:100%;
+padding:15px;`
 
 export const FeedBackContainer=styled.div`
 width:35%;`
 
+export const MainMessage=styled.h1`
+font-weight:600;`
+
 export const ButtonsDiv=styled.div`
 display:flex;
-cursor:pointer;
 `
+
 export const Button=styled.button`
-padding:4px`
+padding:4px;
+margin-right:10px;
+cursor: pointer;`
+
+export const StatisticsContainer=styled.div`
+`
+
+export const StatisticsHeader=styled.h2`
+`
+
+export const Statistics=styled.ul`
+list-style:none;
+`
+
+export const StatisticsItem=styled.li`
+`
