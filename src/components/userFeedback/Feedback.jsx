c0cf60602b83059b6cb5de19f3b425b971532272
SheihMaks <92758256+SheihMaks@users.noreply.github.com>
2022-07-12 01:59:51 +0300

    +

--- src/components/userFeedback/Feedback.jsx
+++ src/components/userFeedback/Feedback.jsx
@@ -1,5 +1,4 @@
-import React from 'react'
-import PropTypes from 'prop-types';
+import React from 'react';
 import {Controls} from '../Controls/FeedbackOptions';
 import {Statistics} from '../feedbackStatistics/statistics';
 import { Section } from '../sectionDoc/Section';
